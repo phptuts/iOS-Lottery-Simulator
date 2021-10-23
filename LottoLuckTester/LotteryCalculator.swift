9140e5d7bf5476b93ac37074075c9ce419d2e68e
phptuts <glaserpower@gmail.com>
2021-10-22 23:35:12 -0700

    committing everything

--- LottoLuckTester/LotteryCalculator.swift
+++ LottoLuckTester/LotteryCalculator.swift
@@ -6,3 +6,127 @@
 //
 
 import Foundation
+
+class LotteryCalculator {
+    
+    private let lottoNum1: Int
+    private let lottoNum2: Int
+    private let lottoNum3: Int
+    private let lottoNum4: Int
+    private let lottoNum5: Int
+    private let powerNumber: Int
+
+    
+    init(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, powerNumber: Int) {
+        self.lottoNum1 = num1
+        self.lottoNum2 = num2
+        self.lottoNum3 = num3
+        self.lottoNum4 = num4
+        self.lottoNum5 = num5
+        self.powerNumber = powerNumber
+    }
+    
+    func runLottery(result: LotteryResult) -> LotteryResult {
+        let lottoPickedNums = Array(0...4).map {  _ in Int.random(in: 1..<71) }
+        let powNum = Int.random(in: 1..<26)
+        
+        let didMatchPowerBall = powNum == self.powerNumber
+        
+        var ballCount = 0;
+        
+        if lottoPickedNums[0] == self.lottoNum1 {
+            ballCount += 1
+        }
+        
+        if lottoPickedNums[1] == self.lottoNum2 {
+            ballCount += 1;
+        }
+        
+        if lottoPickedNums[2] == self.lottoNum2 {
+            ballCount += 1;
+        }
+        
+        if lottoPickedNums[3] == self.lottoNum2 {
+            ballCount += 1;
+        }
+        
+                
+        if didMatchPowerBall && ballCount == 0 {
+            result.addToWinning("2")
+        } else if didMatchPowerBall && ballCount == 1 {
+             result.addToWinning("4")
+        } else if didMatchPowerBall && ballCount == 2 {
+             result.addToWinning("10")
+        } else if didMatchPowerBall == false && ballCount == 3 {
+             result.addToWinning("10")
+        } else if didMatchPowerBall && ballCount == 3 {
+             result.addToWinning("200")
+        } else if didMatchPowerBall == false && ballCount == 4 {
+            result.addToWinning("500")
+        } else if didMatchPowerBall && ballCount == 4 {
+            result.addToWinning("10000")
+        } else if ballCount == 5 && didMatchPowerBall == false {
+            result.addToWinning("1000000")
+        } else if ballCount == 5 && didMatchPowerBall {
+            result.addToWinning("jackpot")
+        } else {
+            result.addToLoss()
+        }
+        
+        result.addToTimes()
+        
+        return result
+    }
+}
+
+class LotteryResult {
+    private var times = 0;
+    private var losses = 0;
+    private var winnings = ["2": 0, "4": 0, "10": 0, "200": 0, "500": 0, "10000": 0, "1000000": 0, "jackpot": 0]
+    
+    public func addToLoss() {
+        self.losses += 1;
+    }
+    
+    public func jackPots() -> Int {
+        return self.winnings["jackpot"] ?? 0
+    }
+    
+    public func addToTimes() {
+        self.times += 1;
+    }
+    
+    public func addToWinning(_ dollar: String) {
+        var total = self.winnings[dollar] ?? 0
+        total += 1
+        self.winnings[dollar] = total
+    }
+    
+    public func getLosses() -> Int {
+        return self.losses
+    }
+    
+    public func spent() -> Int {
+        return self.times * 2
+    }
+    
+    public func totalWinning() -> Int {
+        
+        var total = 0;
+        
+        for (key, value) in self.winnings {
+            if key == "jackpot" {
+                total += 100_000_000 * value
+                continue
+            }
+            
+            guard let addAmount = Int(key) else {
+                continue;
+            }
+            
+            total += addAmount * value
+        }
+        
+        return total
+    }
+}
--- LottoLuckTester/MainViewController.swift
+++ LottoLuckTester/MainViewController.swift
@@ -3,27 +3,173 @@
 //  LottoLuckTester
 //
 //  Created by Noah Glaser on 10/8/21.
-//
+//∫
 
 import UIKit
 
 class MainViewController: UIViewController {
 
+    // has to declared here bcause it will be destroy in memory, because it's in the stack
+    let lottoValidator = TextFieldNumberValidator(min: 1, max: 70)
+    let lottoGoldValidator = TextFieldNumberValidator(min: 1, max: 25)
+    let ticketValidator = TextFieldNumberValidator(min: 1, max: 1000)
+    
+    var running = false
+
+    @IBOutlet weak var winningLabel: UILabel!
+    @IBOutlet weak var spentLabel: UILabel!
+    @IBOutlet weak var totalJackpotLabel: UILabel!
+    @IBOutlet weak var totalYearsLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var timeTypeSegment: UISegmentedControl!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var ticketPerTextField: UITextField!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var yearSlider: UISlider!
+    @IBOutlet weak var lottoNum6: UITextField!
+    @IBOutlet weak var lottoNum5: UITextField!
+    @IBOutlet weak var lottoNum2: UITextField!
+    @IBOutlet weak var lottoNum4: UITextField!
+    @IBOutlet weak var lottoNum3: UITextField!
+    @IBOutlet weak var lottoNum1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        lottoNum1?.delegate = lottoValidator
+        lottoNum2?.delegate = lottoValidator
+        lottoNum3?.delegate = lottoValidator
+        lottoNum4?.delegate = lottoValidator
+        lottoNum5?.delegate = lottoValidator
+        lottoNum6?.delegate = lottoGoldValidator
+        ticketPerTextField?.delegate = ticketValidator
+        refreshButton.setTitle("", for: .normal)
+        randomNumbers()
+        changeButton("Play", color: .orange)
+    }
 
-        // Do any additional setup after loading the view.
+
+    @IBAction func sliderYearChanged(_ sender: Any) {
+        let years = round(yearSlider.value * 10) / 10
+        
+        yearLabel.text = "\(years) Million Years"
+        yearSlider.setValue(years, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onRefreshBtn(_ sender: Any) {
+        randomNumbers()
     }
-    */
+    
+    func randomNumbers() {
+        [lottoNum1, lottoNum2, lottoNum3, lottoNum4, lottoNum5, lottoNum6].forEach {
+            $0.text = String(Int.random(in: 1..<76))
+        }
+    }
 
+    @IBAction func playLotto(_ sender: Any) {
+        if running == true {
+            running = false
+            clearOutput()
+            return
+        }
+        guard let num1 = Int(lottoNum1.text ?? "1") else {
+            return
+        }
+        guard let num2 = Int(lottoNum2.text ?? "1") else {
+            return
+        }
+        guard let num3 = Int(lottoNum3.text ?? "1") else {
+            return
+        }
+        guard let num4 = Int(lottoNum4.text ?? "1") else {
+            return
+        }
+        guard let num5 = Int(lottoNum5.text ?? "1") else {
+            return
+        }
+        
+        guard let num6 = Int(lottoNum6.text ?? "1") else {
+            return
+        }
+        
+        guard let ticketsPerYear = Int(ticketPerTextField.text ?? "1") else {
+            return
+        }
+        
+        
+        let years = Int((round(yearSlider.value * 10) / 10) * 1_000_000)
+            
+        
+        let lottoCal = LotteryCalculator(num1: num1, num2: num2, num3: num3, num4: num4, num5: num5, powerNumber: num6)
+        
+        var lotteryResult = LotteryResult()
+               
+        var yearCount = 0;
+        
+        let queue = DispatchQueue(label: "calculator-queue")
+        self.running = true
+        changeButton("Stop", color: .red)
+
+        queue.async {
+            for i in 1...(ticketsPerYear * years) {
+                lotteryResult = lottoCal.runLottery(result: lotteryResult)
+                if self.running == false {
+                    break
+                }
+                if i % (ticketsPerYear * 50) == 0 {
+                    DispatchQueue.main.async {
+                        yearCount += 50
+                        
+                        self.totalYearsLabel.text = "Years: \(yearCount.withCommas())"
+                        self.totalJackpotLabel.text = "Jackpots: \(lotteryResult.jackPots())"
+                        self.spentLabel.text = "Spent: \(lotteryResult.spent().toMoney())"
+                        self.winningLabel.text = "Winnings: \(lotteryResult.totalWinning().toMoney())"
+                    }
+                }
+            }
+        }
+    }
+    
+    func clearOutput() {
+        totalYearsLabel.text = "Years: "
+        totalJackpotLabel.text = "Jackpots: "
+        spentLabel.text = "Spent: "
+        winningLabel.text = "Winnings: "
+        changeButton("Play", color: .orange)
+    }
+    
+    func changeButton(_ text: String, color: UIColor) {
+        let myNormalAttributedTitle = NSMutableAttributedString(string: text,
+                                                         attributes: [
+                                                            NSAttributedString.Key.foregroundColor : UIColor.white,
+                                                            NSAttributedString.Key.font: UIFont(name: "PermanentMarker-Regular", size: 36.0) ?? UIFont.systemFont(ofSize:   36.0)
+                                                         ])
+        playButton.setAttributedTitle(myNormalAttributedTitle, for: .normal)
+        playButton.layer.cornerRadius = 4
+        playButton.layer.backgroundColor = color.cgColor
+    }
 }
+
+extension Int {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value:self))!
+    }
+    
+    func toMoney() -> String {
+        let formatter = NumberFormatter()
+
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        formatter.decimalSeparator = ","
+        
+        
+
+        return formatter.string(from: NSNumber(value: self))!
+    }
+    
+    
+    
+}
+
+
